ea2caded9a4326247622c77cb3b56b386c5a8aac
Laura <laura-liis.parm@voco.ee>
2026-01-07 10:40:38 +0200

    Ülesanne 1

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,7 +1,27 @@
-import Header from "./Header.jsx";
+import "./App.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseItem from "./ExpenseItem";
 
 function App() {
-  return <Header />;
+  const expenses = [
+    {
+      title: "Car Insurance",
+      price: 294.67,
+      date: new Date(2024, 5, 28),
+    },
+    {
+      title: "New Desk (Wooden)",
+      price: 450,
+      date: new Date(2024, 2, 12),
+    },
+  ];
+
+  return (
+    <>
+      <ExpenseItem expenseData={expenses[0]} />
+      <ExpenseItem expenseData={expenses[1]} />
+    </>
+  );
 }
 
 export default App;
